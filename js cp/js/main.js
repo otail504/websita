7123984a2d043211d7f8dccc85e4051a1557691b
otail504 <52163348+otail504@users.noreply.github.com>
2019-06-27 14:39:42 +0100

    Add files via upload

---
+++ js cp/js/main.js
@@ -0,0 +1,83 @@
+//var myLi = document.getElementsByTagName("h1")[0].style.color="blue";
+//console.log(myLi);  
+
+    /*var a=document.getElementById("test");
+    a.addEventListener("click",function(){
+        this.style.color="red";
+        console.log("test");
+    });*/
+
+
+     
+
+    
+//function changemycolor
+/*function Changecolor(event)
+{ 
+    event.preventDefault();
+var gettext=document.getElementById("test");
+console.log("hhhhhh");
+if ( gettext.style.color=="black")
+{
+    gettext.style.color="red";
+}
+else
+{
+  gettext.style.color="black";
+}
+}*/
+function bold(event)
+{
+event.preventDefault();
+var text=document.getElementById("textarea");
+
+console.log(text.style.fontWeight);
+if(text.style.fontWeight=="bold")
+{
+    text.style.fontWeight="normal";
+}
+else{
+    text.style.fontWeight="bold";
+}
+}
+function italic(event)
+{
+event.preventDefault();
+var itl=document.getElementById("textarea");
+console.log(itl.style.fontStyle);
+if(itl.style.fontStyle=="italic")
+{
+    itl.style.fontStyle="normal";
+}
+else{
+    itl.style.fontStyle="italic";
+}
+}
+function lines()
+{
+
+var line=document.getElementById("textarea");
+console.log(line.style.textDecoration);
+if(line.style.textDecoration=="underline")
+{
+    line.style.textDecoration="none";
+}
+else{
+    line.style.textDecoration="underline";
+}
+}
+function police()
+{
+    var texte=document.getElementById("textarea");
+    var size=document.getElementById("select").value;
+    texte.style.fontSize=size;
+    console.log(texte.style.fontSize);
+}
+function policetyp()
+{
+    var texte=document.getElementById("textarea");
+    var font=document.getElementById("select11").value;
+    texte.style.fontFamily=font;
+    console.log(texte.style.fontFamily);
+}
+
